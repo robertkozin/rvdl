017e37d63200e2be7d881ef9696e7c53fe894ef8
Robert Kozin <robert@robertkozin.com>
2020-12-12 19:00:37 -0500

    Fix oopsie from previous refactor commit

--- pkg/util/env.go
+++ pkg/util/env.go
@@ -5,23 +5,23 @@ import (
 	"strconv"
 )
 
-func EnvString(key string, val string) string {
+func EnvString(key string, defaultVal string) string {
 	val, ok := os.LookupEnv(key)
 	if !ok {
-		return val
+		return defaultVal
 	}
 
 	return val
 }
 
-func EnvBool(key string, val bool) bool {
+func EnvBool(key string, defaultVal bool) bool {
 	rawVal, ok := os.LookupEnv(key)
 	if !ok {
-		return val
+		return defaultVal
 	}
 	val, err := strconv.ParseBool(rawVal)
 	if err != nil {
-		return val
+		return defaultVal
 	}
 
 	return val
